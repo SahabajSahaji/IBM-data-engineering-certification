99d3e83993de9dacf3a288f9a1da25a9b914f285
SahabajSahaji <sahabajsahaji@gmail.com>
2026-05-16 00:04:30 +0530

    WebScrapingProject

---
+++ APIs and Data Collection/API.py
@@ -0,0 +1,10 @@
+import requests
+
+url_get='http://httpbin.org/get'
+payload={"name":"Joseph","ID":"123"}
+
+r=requests.get(url_get,params=payload)
+
+print(r.status_code)
+print(r.text)
+print(r.json)
